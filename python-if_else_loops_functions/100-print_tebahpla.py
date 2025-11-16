20b57f554b325ee573300773c53792d79718fae0
root <asadliamir800@gmail.com>
2025-11-16 07:36:35 +0000

    Changes Saved

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -3,10 +3,10 @@
 # 90 - Z
 i = 122
 num = 0
+for i in range()
 while num <= 25:
     if (num % 2 == 1):
-        print(chr(i-32), end="")
-    else:
-        print(chr(i), end="")
+        i=i-32
+    print(chr(i))
     i-=1
     num+=1
