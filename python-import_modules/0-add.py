5f0aeaefdb33cac967e134bd76b4130fbbe9ae9a
root <asadliamir800@gmail.com>
2025-11-18 07:18:39 +0000

    Changes Saved

---
+++ python-import_modules/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def add():
+    a = 1
+    b = 2
+    add_0 = a + b
+    print("{} + {} = {}".format(a, b, add_0))
+    return (a + b)
+
+add()
---
+++ python-import_modules/README.md
@@ -0,0 +1,1 @@
+In this directory we will discover the modules used in python
