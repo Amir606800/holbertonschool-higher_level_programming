bfec38e9a74654e81b67e51921e2da12a13c4f19
root <asadliamir800@gmail.com>
2025-11-20 13:46:30 +0000

    AWDA

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     uz = len(my_list)
-    max_int = my_list[0]
     if uz == 0:
         return None
     else:
+        max_int = my_list[0]
         for i in my_list:
             if max_int < i:
                 max_int = i
