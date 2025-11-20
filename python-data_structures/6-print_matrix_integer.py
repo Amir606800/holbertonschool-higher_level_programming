9f48fdd1493d187ee33e1403405babae805e1642
root <asadliamir800@gmail.com>
2025-11-20 13:06:49 +0000

    After a long time first commit message

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
-        for j in i:
-            print("{:d}".format(j), end=" ")
-        print("\n".format())
+        for j in range(len(i)):
+            print("{:d}".format(j), end="")
+            if j < len(i) - 1:
+                print("{:d}".format(j), end=" ")
+        print()
