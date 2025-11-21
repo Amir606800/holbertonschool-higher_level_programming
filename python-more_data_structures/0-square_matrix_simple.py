db58e4831cfc79c8152bcc70dc61881271729dfe
root <asadliamir800@gmail.com>
2025-11-21 06:54:43 +0000

    ASDAWDdawdwddaDWA

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -4,12 +4,3 @@ def square_matrix_simple(matrix=[]):
     for row in matrix:
         new_matrix += [[x**2 for x in row]]
     return new_matrix
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-new_matrix = square_matrix_simple(matrix)
-print(new_matrix)
-print(matrix)
