00f5e0206ea6e0018976c5745b9e0073e646cd3b
root <asadliamir800@gmail.com>
2025-11-21 11:06:40 +0000

    AWndawhdawdWd

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if isinstance(roman_string, str) or roman_string == None:
+        return 0
+    nums = { 
+    "I": 1,
+    "V": 5,
+    "X": 10,
+    "L": 50,
+    "C": 100,
+    "D": 500,
+    "M": 1000
+    }
+    number = 0
+    for i in range(len(roman_string)-1):
+        curr = roman_string[i]
+        next = roman_string[i+1]
+        if nums[curr] <= nums[next]:
+            number += nums[curr]
+        else:
+            number -= nums[curr]
+    return number
