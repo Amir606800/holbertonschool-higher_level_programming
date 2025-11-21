6b556f740699feba567b410e9d7583de6f6209d7
root <asadliamir800@gmail.com>
2025-11-21 07:48:20 +0000

    Changes Saved

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-def common_elements(set_1, set_2):
+def only_diff_elements(set_1, set_2):
     new_list = []
     for i in set_1:
         if i not in set_2:
+            new_list.append(i)
+    for i in set_2:
+        if i not in set_1:
             new_list.append(i)
     return new_list
