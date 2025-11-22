1dcf69f369ec6f6f0bcfda8c0cf49b58c6268513
root <asadliamir800@gmail.com>
2025-11-22 07:25:28 +0000

    awdawdAW

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary == None:
+    if a_dictionary == None or len(a_dictionary) == 0:
         return None
     items = list(a_dictionary.items())
     max_key, max_val = items[0]
