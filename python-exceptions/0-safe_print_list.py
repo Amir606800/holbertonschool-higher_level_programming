6f5f4658de3c5f84790e2c8f25c89f5c13e4c9e6
root <asadliamir800@gmail.com>
2025-11-22 08:37:56 +0000

    another try

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
-        for i in my_list[:x]:
-            print(i, end="")
-        return x[-1]
-    except TypeError:
+        for i in range(x):
+            print(my_list[i], end="")
         print()
-        return my_list[-1]
+        return x
+    except IndexError:
+        print()
+        return i
