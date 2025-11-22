554b3a15229bd894a87deca7be0d138fed7ef8e1
root <asadliamir800@gmail.com>
2025-11-22 07:56:33 +0000

    Fixed the Roman Numbers

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,22 +1,16 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if isinstance(roman_string, str) or roman_string == None:
+    if not(isinstance(roman_string, str)) or roman_string is None:
         return 0
-    nums = { 
-    "I": 1,
-    "V": 5,
-    "X": 10,
-    "L": 50,
-    "C": 100,
-    "D": 500,
-    "M": 1000
-    }
+    nums = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     number = 0
     for i in range(len(roman_string)-1):
         curr = roman_string[i]
         next = roman_string[i+1]
-        if nums[curr] <= nums[next]:
+        if nums[curr] >= nums[next]:
             number += nums[curr]
         else:
             number -= nums[curr]
+    if roman_string:
+        number += nums[roman_string[-1]]
     return number
