a0e7afa1bf9820174eb2689d24279b41d3820f1c
root <asadliamir800@gmail.com>
2025-11-24 09:20:48 +0000

    Drawing the rectangle Eval Methods Attempt#2

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -60,9 +60,4 @@ class Rectangle:
         return drawing + self.width * "#"
 
     def __repr__(self):
-        drawing = ""
-        if self.width == 0 or self.height == 0:
-            return drawing
-        for i in range(self.height-1):
-            drawing = drawing + self.width * "#" + "\n"
-        return drawing + self.width * "#"
+        return f"Rectangle({self.__width}, {self.height})"
