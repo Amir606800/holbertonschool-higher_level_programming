2f90ba9f8c67fbd9056003384a5b474d52f374b8
root <asadliamir800@gmail.com>
2025-11-25 18:48:13 +0000

    Something

---
+++ python-inheritence/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" This is the creation of the method """
+
+
+def lookup(obj):
+    """ We just return the value of dictionary of object """
+    return list(obj.__dict__)
---
+++ python-inheritence/README.md
@@ -0,0 +1,2 @@
+This project will cover up inheritence topic
+
