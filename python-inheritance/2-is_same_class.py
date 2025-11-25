a36a77eb8535aa43b0ba712993c8054321033953
root <asadliamir800@gmail.com>
2025-11-25 19:25:07 +0000

    Object class checker Attempt#1

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Create the checker def function first """
+
+
+def is_same_class(obj, a_class):
+    """ Now we will use type in order to check if it is insance of a class """
+
+    return type(obj) is a_class
