7614e3ed2efa623ee60b5829fccb2058de3bdb89
root <asadliamir800@gmail.com>
2025-11-25 19:55:39 +0000

    Raised an Error #1

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Creating an empty class """
+
+
+class BaseGeometry:
+    """ Write pass in order to create an empty class """
+
+    def area(self):
+        raise Exception("area() is not implemented")
