5b621f3d5cf1f05cb45abb9d6cd9959cd98eb160
root <asadliamir800@gmail.com>
2025-11-26 09:56:31 +0000

    From Json to String Attempt#1

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Converting the format of the inputs """
+import json
+
+
+def from_json_string(my_obj):
+    """ Now return the converted output """
+
+    return json.loads(my_obj)
