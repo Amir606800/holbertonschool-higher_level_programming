4e3edeff73fbd1c63613464a6596068c24f46881
root <asadliamir800@gmail.com>
2025-11-26 07:30:11 +0000

    Awdad

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" The first thing is import the Rectangle class to this code """
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Now let's init the class """
+
+    def __init__(self, size):
+        super().integer_validator("size", size)
+        super().__init__(size, size)
