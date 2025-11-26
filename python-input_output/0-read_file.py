f691119a34c9fc8abf69ff7a37e17aa2be59f032
root <asadliamir800@gmail.com>
2025-11-26 08:22:37 +0000

    Atempt#2 for the file read

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 """ Writing the function for the file to be open """
 
+
 def read_file(filename=""):
-    """ We specified the filename and open it with "with" statement and the "open" object. "with" will handle the closing of the file. """
+    """
+       We specified the filename and open it with
+       "with" statement and the "open" object.
+       "with" will handle the closing of the file.
+    """
+
     with open(filename) as f:
         print(f.read())
