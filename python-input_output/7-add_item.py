2d5c34c782513cab6e2c0a962be2a345aa206074
root <asadliamir800@gmail.com>
2025-11-27 07:42:53 +0000

    Let's try without using the 6th function

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
-import json
+""" Frist import the necessary modules """
+import json  # To use the json formatting in our code
+import sys  # In order to get the arguments from the command
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
+all_args = sys.argv[1:]
+save_to_json_file(all_args, "add_item.json")
