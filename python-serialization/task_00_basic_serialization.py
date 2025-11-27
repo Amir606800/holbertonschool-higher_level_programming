ec07ac4856eb553ee79a7327173b5671e2a70fc0
root <asadliamir800@gmail.com>
2025-11-27 13:38:26 +0000

    Task1

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -14,5 +14,5 @@ def serialize_and_save_to_file(data, filename):
 
 def load_and_deserialize(filename):
     with open(filename, "r") as f:
-        return json.loads(f)
+        return json.load(f)
 
