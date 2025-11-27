7a9e116900a384d254f642faac35ff069d8e7cb1
root <asadliamir800@gmail.com>
2025-11-27 07:47:27 +0000

    Added the 6th function in order to get the prev list in json Attmept#2

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,4 +6,7 @@ save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 all_args = sys.argv[1:]
+if load_from_json_file("add_item.json"):
+    prev_list = load_from_json_file("add_item.json")
+    all_args = all_args.extend(prev_list)
 save_to_json_file(all_args, "add_item.json")
