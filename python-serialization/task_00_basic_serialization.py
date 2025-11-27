74673029261a34ffadcd57bd6f29731b4b5f6657
root <asadliamir800@gmail.com>
2025-11-27 13:27:14 +0000

    Task1

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,18 @@
+#!/usr/bin/pyhton3
+""" Serialization and deserialization """
+import json
+
+
+def serialize_and_save_to_file(data, filename):
+    """
+      Serialization with dump
+      because we have data
+      and the destination file
+    """
+    with open(filename, "w") as f:
+        f.dump(data, f)
+
+def load_and_deserialize(filename):
+    with open(filename, "r") as f:
+        return f.loads(f)
+
