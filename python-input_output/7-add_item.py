a7836f8782fa650b018e5abe0015f424b1202040
root <asadliamir800@gmail.com>
2025-11-27 08:17:21 +0000

    Change the entire code into normal form

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """ Frist import the necessary modules """
-import json  # To use the json formatting in our code
 import sys  # In order to get the arguments from the command
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 all_args = sys.argv[1:]
-if load_from_json_file("add_item.json"):
+try:
     prev_list = load_from_json_file("add_item.json")
-    all_args = all_args.extend(prev_list)
-save_to_json_file(all_args, "add_item.json")
+except FileNotFoundError:
+    prev_list = []
+prev_list.extend(all_args)
+save_to_json_file(prev_list, "add_item.json")
