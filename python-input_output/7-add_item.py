477a21d630b1671d372882c98f021c77596066ee
root <asadliamir800@gmail.com>
2025-11-27 08:19:36 +0000

    Change the entire code into normal form Attmept#2

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,8 +6,15 @@ load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 all_args = sys.argv[1:]
 try:
+    """ Try if the list is aviable then load """
     prev_list = load_from_json_file("add_item.json")
 except FileNotFoundError:
+    """ If not then create an empty list """
     prev_list = []
+"""
+  At the end we will just
+  extend the list and save
+  the new args into it
+"""
 prev_list.extend(all_args)
 save_to_json_file(prev_list, "add_item.json")
