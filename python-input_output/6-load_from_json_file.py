6e186821a76efcad90e7d73366f09f682a0e2a30
root <asadliamir800@gmail.com>
2025-11-27 07:18:33 +0000

    Task 6 the load from the json file Attempt#1

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" We will use the json library and open it with open() """
+import json
+
+
+def load_from_json_file(filename):
+    """ Here we will use json.load in order to parse the json format into the string format """
+
+    with open(filename) as f:
+        return json.load(f)
