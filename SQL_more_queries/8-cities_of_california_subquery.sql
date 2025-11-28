5638e5800d0ce7aba5098721b23b2e190a6f2b03
root <asadliamir800@gmail.com>
2025-11-28 20:43:30 +0000

    State and city

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- Selecting cities from states
-SELECT s.id, c.name FROM states s, cities c WHERE c.state_id = s.id and s.name = 'California' ORDER BY c.id ASC;
+SELECT c.id, c.name FROM states s, cities c WHERE c.state_id = s.id and s.name = 'California' ORDER BY c.id ASC;
