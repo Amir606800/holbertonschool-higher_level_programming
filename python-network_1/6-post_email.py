b4c2f1245fbe3c5e5d387235c8141e2d739a94af
root <asadliamir800@gmail.com>
2025-12-01 16:37:59 +0000

    wbaidw

---
+++ python-network_1/6-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Posting email with requests """
+import requests
+import sys
+
+
+if len(sys.argv) >= 3:
+    url = sys.argv[1]
+    email = sys.argv[2]
+    headers = {'cfclearance': 'true'}
+    res = requests.post(url, data={"email": email}, headers=headers)
+    print(f"Your email is: {res.text}")
+else:
+    print()
