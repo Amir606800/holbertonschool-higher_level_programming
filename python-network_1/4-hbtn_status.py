e8248ee1f99ac148494e2dbe1257d75a3f401c5a
root <asadliamir800@gmail.com>
2025-12-01 16:16:59 +0000

    bwudavwd

---
+++ python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" New method: Using requests """
+import requests
+
+
+r = requests.get("https://intranet.hbtn.io/status")
+#print(r)
+print("Body response:")
+#print(f"\t- type: {type(r)}")
+print(f"\t- content: {r.text}")
