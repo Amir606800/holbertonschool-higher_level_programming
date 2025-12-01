b7614cc71ed72fc4b4b2ae77d75d2c914df208d3
root <asadliamir800@gmail.com>
2025-12-01 16:49:01 +0000

    dawdtsweyh

---
+++ python-network_1/7-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Error handling """
+import requests
+import requests.exceptions as re
+import sys
+
+
+if len(sys.argv) < 2:
+    print()
+else:
+    url = sys.argv[1]
+    try:
+        res = requests.get(url)
+        print(res.text)
+    except re.HTTPError as errh:
+        print(f"Error code: {errh.code}")
