36059a6f2838b9969d87e8d99d17a2e5440578fd
root <asadliamir800@gmail.com>
2025-12-01 10:34:32 +0000

    Salam

---
+++ python-network_1/1-hbtn_header.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Comment """
+from urllib.request import Request, urlopen
+import sys
+
+
+if len(sys.argv) >= 2:
+    argument = sys.argv[1]
+    req = argument
+    with urlopen(req) as web:
+        print(web.headers["X-Request-Id"])
+else:
+    print()
+
+
