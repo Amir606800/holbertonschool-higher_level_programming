177b8d1246cee843490be2e4a30ed0c0f59bdac1
root <asadliamir800@gmail.com>
2025-12-04 11:22:50 +0000

    aywdfgiawvd

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -5,7 +5,7 @@ import os
 
 def generate_invitations(template_content, attendees):
 
-    if len(template_content) == 0 or not isinstance(template_content, str):
+    if not isinstance(template_content, str) or len(template_content) == 0:
         print("Template is empty, no output files generated.")
         return
 
